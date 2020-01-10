fba9d4099971188db9efedeb75ee4871ed06546b
vincent7q <vincent7q@gmail.com>
2020-01-10 15:29:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,12 +38,7 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     keywords='deribit api',
